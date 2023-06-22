e7aef7d2a1a4d261cc250bfe780f69c3c7501ed5
Sagarsmogare123 <sagarsmogare123@gmail.com>
2023-06-22 19:42:33 +0530

    working properly added test cases, GET, POST view

--- myproject/myapp/tests.py
+++ myproject/myapp/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+from .models import Category, Product
 
-# Create your tests here.
+class CategoryTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
+        self.category = Category.objects.create(name='Test Category')
+        self.url = reverse('category-list')
+
+    def test_create_category(self):
+        data = {'name': 'New Category'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Category.objects.count(), 2)
+
+    def test_retrieve_category(self):
+        url = reverse('category-detail', args=[self.category.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class ProductTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.force_authenticate(user=self.user)
+        self.category = Category.objects.create(name='Test Category')
+        self.product = Product.objects.create(name='Test Product', category=self.category, price=10.0)
+        self.url = reverse('product-list')
+
+    def test_create_product(self):
+        data = {'name': 'New Product', 'category': self.category.id, 'price': 20.0}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Product.objects.count(), 2)
+
+    def test_retrieve_product(self):
+        url = reverse('product-detail', args=[self.product.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
